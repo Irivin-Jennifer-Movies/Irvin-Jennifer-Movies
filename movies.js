a8f2a81ba894542a5b07a3fd9342c23d0a47de83
Irvin Ruiz <ruizirvin084@gmail.com>
2022-02-18 15:51:58 -0600

    added appending movie data to html

--- movies.js
+++ movies.js
@@ -1,11 +1,33 @@
-
-
+function getAllMovies() {
 fetch("https://cute-savory-grandparent.glitch.me/movies").then(response =>
-    response.json().then(data => {
-        for (let i =0; i < 16; i++){
-            console.log(data[i].title);
-        }
-        $('p').css("display", "none")
-    })
-);
+	response.json().then(data => {
+		for (let i = 0; i < 16; i++) {
+			if (data[i].title === undefined) {
+			} else {
+				let title = data[i].title.toUpperCase();
 
+				//language=HTML
+				$('#card-div').append(`
+                    <div class="card-deck">
+                        <div class="card" id="movie0" style="width: 18rem;">
+                            <img class="card-img-top" style="height: 429px" src="${data[i].poster}" alt="Card image cap">
+                            <div class="card-body">
+                                <h5 class="card-title">${title}</h5>
+                            </div>
+                            <ul class="list-group list-group-flush">
+                                <li class="list-group-item" style="height: 121px">${data[i].actors}</li>
+                                <li class="list-group-item">${data[i].director}</li>
+                                <li class="list-group-item">${data[i].genre}</li>
+                                <li class="list-group-item">${data[i].plot}</li>
+                            </ul>
+                        </div>
+                    </div>
+				`)
+			}
+		}
+		$('p').css("display", "none")
+	})
+);
+}
+
+getAllMovies();
