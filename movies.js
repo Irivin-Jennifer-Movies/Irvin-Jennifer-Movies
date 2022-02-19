00cc47658f6104dc43452b643d9aa467e6e03a49
Jennifer Deleon <jennifer.deleon404@gmail.com>
2022-02-18 23:14:30 -0600

    fixed small error with loading posters

--- movies.js
+++ movies.js
@@ -1,26 +1,26 @@
 const url = "https://cute-savory-grandparent.glitch.me/movies"
 function getAllMovies() {
-fetch("https://cute-savory-grandparent.glitch.me/movies").then(response =>
+fetch(url).then(response =>
 	response.json().then(data => {
         $('#card-div').html("")
 		for (let i = 0; i < data.length; i++) {
 			if (data[i].title === undefined) {
 			} else {
 				let title = data[i].title.toUpperCase();
-
+                let imgTag = (data[i].poster) ? `<img className="card-img-top" style="height: 429px" src="${data[i].poster}" alt="Card image cap">` : "";
 				//language=HTML
 				$('#card-div').append(`
                     <div class="card-deck">
                         <div class="card" id="movie0" style="width: 18rem;">
-                            <img class="card-img-top" style="height: 429px" src="${data[i].poster}" alt="Card image cap">
+                            ${imgTag}
                             <div class="card-body">
                                 <h5 class="card-title">${title}</h5>
                             </div>
                             <ul class="list-group list-group-flush">
-                                <li class="list-group-item" style="height: 121px">${data[i].actors}</li>
-                                <li class="list-group-item">${data[i].director}</li>
-                                <li class="list-group-item">${data[i].genre}</li>
-                                <li class="list-group-item">${data[i].plot}</li>
+                                <li id="actors" class="list-group-item" style="height: 121px">${data[i].actors}</li>
+                                <li id="directors" class="list-group-item">${data[i].director}</li>
+                                <li id="genre" class="list-group-item">${data[i].genre}</li>
+                                <li id="plot" class="list-group-item">${data[i].plot}</li>
                             </ul>
                         </div>
                     </div>
@@ -33,7 +33,7 @@ fetch("https://cute-savory-grandparent.glitch.me/movies").then(response =>
 }
 function newMovies() {
     const userInput = {
-        title: "hello",
+        title: "",
         actors: "",
         genre: ""
     };
